d410de9eab1f8a8aea87884a84f2a06c78598ddf
jumpdart <markhoward000@gmail.com>
2020-06-28 11:05:54 -0400

    base files

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+// webpack.config.js
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+
+module.exports = {
+    module: {
+        rules: [
+            {
+                test: /\.vue$/,
+                loader: 'vue-loader'
+            },
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    'vue-style-loader',
+                    'css-loader'
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new VueLoaderPlugin()
+    ]
+}
